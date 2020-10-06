3e235be041b2499880d4d91cdab57b42a3464430
markkovari <kovarimarkofficial@gmail.com>
2020-10-06 22:41:46 +0200

    fix: database connection envvar readings

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserService } from './user/user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        ({
+          entities: [],
+          type: 'mariadb',
+          host: configService.get('DATABASE_HOST', 'localhost'),
+          port: configService.get('DATABASE_PORT', 3306),
+          username: configService.get('DATABASE_USER', 'root'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_DATABASE'),
+          synchronize: true,
+        })
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule { }
